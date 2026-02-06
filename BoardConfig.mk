051652ebf6c9d4da0b6bd052c33d50060596fd91
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-02-06 11:50:37 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,24 +1,26 @@
-# Include the common chipset BoardConfig
+# Include common chipset config
 include device/tecno/mt6789-common/BoardConfigCommon.mk
 
-# Path Definitions base sa manifest mo
+# PATHS BASE SA MANIFEST MO
+# LG7n-kernel = DTB, DTBO, at Modules
+# common-kernel = Image.gz
 KERNEL_PATH := device/tecno/LG7n-kernel
 COMMON_KERNEL_PATH := device/millennium/common-kernel
 
-# Boot image & DTB (Galing sa LG7n-kernel folder)
+# Boot image & DTB
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
 BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
 
-# Kernel Image (Dito galing sa common-kernel folder)
+# Kernel Image Location
 TARGET_NO_KERNEL_OVERRIDE := true
 LOCAL_KERNEL := $(COMMON_KERNEL_PATH)/Image.gz
 
-# I-copy ang kernel image para sa final build
+# Copy the image so the build system sees it as the kernel
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-# Kernel modules (Base sa screenshot mo: dtb, ramdisk, vendor_dlkm)
+# Modules Safety Check (Para hindi sumablay ang zip packaging)
 ifneq ($(wildcard $(KERNEL_PATH)/ramdisk/modules.load),)
     BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_PATH)/ramdisk/modules.load))
     BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(KERNEL_PATH)/ramdisk/, $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD))
@@ -29,10 +31,8 @@ ifneq ($(wildcard $(KERNEL_PATH)/vendor_dlkm/modules.load),)
     BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(KERNEL_PATH)/vendor_dlkm/*.ko)
 endif
 
-# OTA Assertions
+# OTA & SEPolicy
 TARGET_OTA_ASSERT_DEVICE := LG7n,TECNO-LG7n,lg7n
-
-# SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit proprietary files
