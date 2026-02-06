b37fc715636492d8e649c88fd0d544f52eb690b9
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-02-06 10:38:56 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -53,4 +53,4 @@ $(call inherit-product, device/tecno/mt6789-common/common.mk)
 $(call inherit-product, vendor/tecno/LG7n/LG7n-vendor.mk)
 
 # Signingkey
--include vendor/lineage-priv/keys/keys.mk
+-include vendor/miku-priv/keys/keys.mk
