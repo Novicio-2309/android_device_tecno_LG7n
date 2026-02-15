b74139af0cfe0ef3fcf0bc5a469193862ddf54bf
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-02-16 01:24:54 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ TARGET_OTA_ASSERT_DEVICE := LG7n,TECNO-LG7n,lg7n
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
 
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
