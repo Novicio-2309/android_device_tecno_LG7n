f20c85779e24f3eb2d690639d58d0cb93f1c0900
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-06-03 14:56:16 +0800

    Change signing key inclusion method in device.mk

--- device.mk
+++ device.mk
@@ -49,5 +49,5 @@ $(call inherit-product, device/tecno/mt6789-common/common.mk)
 # Inherit the proprietary files
 $(call inherit-product, vendor/tecno/LG7n/LG7n-vendor.mk)
 
-# Signingkey
--include vendor/infinity-priv/keys/keys.mk
+# Signing
+$(call inherit-product, vendor/infinity-priv/keys/keys.mk)
